5be782414b823d568850955b31e635fb641c1dc9
phunc20 <wucf20@gmail.com>
2022-05-31 00:48:49 +0700

    notes taken on Hungarian algorithm

---
+++ thematic/hungarian_algorithm/assignment_problem.jl
@@ -0,0 +1,37 @@
+### A Pluto.jl notebook ###
+# v0.19.5
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 0b7a9340-e023-11ec-16d0-5da76a16e9aa
+md"""
+> s.t. each row is assignment to at most one column, and each column to at most one row.
+
+is equiv. to saying
+> $\sum_{i} \sum_{j} X_{i,j} = n,$ where $n$ equals the `#(rows of X)`
+"""
+
+# ╔═╡ a6fe80d3-a755-4699-a2c5-40154c7b6a9d
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─0b7a9340-e023-11ec-16d0-5da76a16e9aa
+# ╠═a6fe80d3-a755-4699-a2c5-40154c7b6a9d
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
