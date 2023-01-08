4a1652fdbb60c2886637abad91b5c7496639f51d
phunc20 <wucf20@gmail.com>
2023-01-09 01:14:55 +0700

    add pulp to assignment problem algorithm comparison

--- thematic/assignment_problem/README.md
+++ thematic/assignment_problem/README.md
@@ -1,4 +1,7 @@
-
+## `comparison.py`
+1. [ ] Add flag to max/min the problem
+2. [ ] Add solution using PuLP
+2. [ ] 
 
 
 
@@ -7,7 +10,7 @@
 ## Q&A
 1. The Hungarian algorithm feels sophisticated. Is there anything which assured Mr. Kuhn of its success, before his
    diving into designing such complex algorithm? I mean, it will be a huge waste if, after much struggling and
-   effort, we end up with a solution with the same worst complexity of $O(n!)$.
+   effort, we end up with a solution with the same worst-case complexity of $O(n!)$.
 
 
 
--- thematic/assignment_problem/comparison.py
+++ thematic/assignment_problem/comparison.py
@@ -4,19 +4,61 @@ import math
 import time
 
 import numpy as np
+from pulp import (
+    LpProblem,
+    LpVariable,
+    LpMaximize,
+    lpSum,
+    GLPK,
+)
 from scipy.optimize import linear_sum_assignment
 from tqdm import tqdm
+
 
 def brute_force(cost):
     k = cost.shape[0]
     maximum = -np.inf
     #for perm in permutations(range(k)):
     for perm in tqdm(permutations(range(k)), total=np.math.factorial(k)):
+        #import ipdb; ipdb.set_trace()
         somme = cost[range(k), perm].sum()
         if somme >= maximum:
             maximum = somme
             best_perm = perm
     return best_perm
+
+
+def pulp_way(cost, *, verbose=False, solver=GLPK, debug=False):
+    m, n = cost.shape
+    model = LpProblem(name="linear-assignment", sense=LpMaximize)
+    X = []
+    for i in range(m):
+        X.append([])
+        for j in range(n):
+            X[i].append(
+                LpVariable(name=f"X[{i},{j}]", cat="Binary")
+            )
+    # Constraints
+    for i in range(m):
+        model += (lpSum(X[i]) == 1, f"row_{i}_sum")
+    for j in range(n):
+        model += (lpSum(X[i][j] for i in range(m)) == 1, f"col_{j}_sum")
+    obj_func = lpSum(
+        cost[i,j]*X[i][j] for i in range(m) for j in range(n)
+    )
+    model += obj_func
+    status = model.solve(solver=solver(msg=False))
+    if debug:
+        print(f"{model.objective.value() = }")
+    perm = []
+    for k, var in enumerate(model.variables()):
+        if debug:
+            print(f"{var.name} = {var.value()}")
+        if var.value() == 1:
+            j = k % n
+            perm.append(j)
+    #print(f"{perm = }")
+    return perm
 
 
 def do_nothing_loop(cost):
@@ -55,7 +97,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
     deactivate = args.deactivate
     print(f"args.n = {args.n}")
-    exit(1)
     n = args.n if isinstance(args.n, list) else [args.n]
     show_more = args.show_more
 
@@ -69,7 +110,8 @@ if __name__ == "__main__":
             start = time.time()
             best_perm = brute_force(cost)
             end = time.time()
-            print(f"Brute force took {end-start:.9f} sec.")
+            #print(f"Brute force took {end-start:.9f} sec.")
+            print(f"Brute force           took {end-start:.9f} sec, i.e. {(end-start)*10**6:,.0f} ms")
             # convert to np.array for easier visual comparison
             #print(f"best_perm = {best_perm}")
             print(f"best_perm =  {np.array(best_perm)}")
@@ -79,10 +121,19 @@ if __name__ == "__main__":
         start = time.time()
         _, scipy_perm = linear_sum_assignment(cost, maximize=True)
         end = time.time()
-        print(f"linear_sum_assignment() took {end-start:.9f} sec, i.e. {(end-start)*10**6:.0f} ms")
+        print(f"linear_sum_assignment took {end-start:.9f} sec, i.e. {(end-start)*10**6:,.0f} ms")
         print(f"scipy_perm = {scipy_perm}")
         print(f"sum = {cost[range(cost.shape[0]), scipy_perm].sum()}")
         print()
+
+        start = time.time()
+        pulp_perm = pulp_way(cost)
+        end = time.time()
+        print(f"pulp_way              took {end-start:.9f} sec, i.e. {(end-start)*10**6:,.0f} ms")
+        print(f"pulp_perm = {np.array(pulp_perm)}")
+        print(f"sum = {cost[range(cost.shape[0]), pulp_perm].sum()}")
+        print()
+
 
         if show_more:
             start = time.time()
